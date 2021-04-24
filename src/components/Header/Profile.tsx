124243721cf05fee68d31bb468385f3170d620dc
crlne <carol.lages@outlook.com>
2021-04-24 19:46:02 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,20 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 
-export default function Profile() {
+
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData =  true }: ProfileProps) {
     return (
         <Flex align="center">
-        <Box mr="4" textAlign="right">
-            <Text>Caroline Ferreira</Text>
-            <Text clor="gray.300" fontSize="small">carol.lages@outlook.com</Text>
-        </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                <Text>Caroline Ferreira</Text>
+                <Text clor="gray.300" fontSize="small">carol.lages@outlook.com</Text>
+            </Box>
+            )}
 
         <Avatar size="md" nome="Caroline Ferreira" src="http://github.com/crlne.png" />
     </Flex>
