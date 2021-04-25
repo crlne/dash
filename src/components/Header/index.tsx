7fb5e6b9bd5e13b2b55f270434e8fcd36253c10d
crlne <carol.lages@outlook.com>
2021-04-25 18:17:44 -0300

    context sidebar responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Icon } from '@chakra-ui/core'
+import { Flex, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext'
 
 import Logo from './Logo'
 import NotificationNav from './NotificationNav'
@@ -6,12 +9,22 @@ import Profile from './Profile'
 import SearchBox from './SearchBox'
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
     return (
         <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
+
+            { isWideVersion && (
+                <IconButton aria-label="Open navigation" icon={<Icon as={RiMenuLine} />}
+                fontSize="24" variant="unstyled" onClick={onOpen} mr="2" 
+                >
+
+                </IconButton>
+            )}
             
             <Logo />
             
